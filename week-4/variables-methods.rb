0645974e610d33c3ef45a190bbbae500b32848f4
Ryan Zell <ryanzell36@gmail.com>
2015-10-21 14:09:43 -0500

    Variable-methods update

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -1,3 +1,30 @@
-Sup
+def do_something(a, b, c)
+end
 
-[4.3.1 Return a Formatted Address](https://github.com/RedZulu/phase-0/blob/master/week-4/simple-string.rb)
+
+<li><a href="https://github.com/RedZulu/phase-0/blob/master/week-4/simple-string.rb">4.3.1 Return a Formatted Address</a></li>
+
+<li><a href="https://github.com/RedZulu/phase-0/blob/master/week-4/math/my_solution.rb">4.3.2 Defining Math Methods</a></li>
+
+#!/usr/bin/env ruby
+=begin
+
+How do you define a local variable?
+A local variable is defined within its own function block.
+
+How do you define a method?
+You have to define a variable using the def function followed by the name of the method and then within parenthesis the parameters of said method.
+
+What is the difference between a local variable and a method?
+A method can receive inputs from a user, variables, or methods outside of itself to fill its parameters. A local variable can only be defined as one variable.
+
+How do you run a ruby program from the command line?
+type in irb or ruby your_program.rb
+
+How do you run an RSpec file from the command line?
+type rspec your_tester_program.rb
+
+What was confusing about this material? What made sense?
+I think the address problem has a bug in it the rspec for it wasn't working for me properly. I kept receiving a name identifier error.
+
+=end
