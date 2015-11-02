7d2b537c39fa1960df708e4a7cfcae2db1f02da1
RedZulu <ryanzell36@gmail.com>
2015-11-02 09:34:30 -0600

    week-5 update groups

--- week-5/account-groups/acct_groups.rb
+++ week-5/account-groups/acct_groups.rb
@@ -1,4 +1,10 @@
-seperate names into x groups of z size
+#seperate names into x groups of z size
+#create a name array
+#define group sort method
+#create empty hash to store names
+#split names into groups using concat and a parameter
+#Print out every concat
+
 
 names = ["Caroline Artz", 
 "Syema Ailia", 
@@ -87,3 +93,47 @@ end
   p name
 end
 unique_group(names)
+
+#refactor
+=begin
+	
+I tried several things but none of them seemed to work properly.
+	
+=end
+
+#driver
+
+puts "What names would you like to add to commit to groups?
+Please seperate names with commas."
+new_people = gets.chomp
+p unique_group(new_people)
+
+#reflect
+
+=begin
+	
+What was the most interesting and most difficult part of this challenge?
+
+Rememering that I could concat things.
+
+Do you feel you are improving in your ability to write pseudocode and break the problem down?
+
+Yes definitely, but it is hard for me to tell what is proper sudo code. Looking over the best practices
+I feel as if I can understand myself better not following them percisely. I do know this needs to change
+for others.
+
+Was your approach for automating this task a good solution? What could have made it even better?
+
+I feel as if it was adiquite, but I think that I could have found a way to count the number of variables in the array
+and define a formula to break them up evenly instead of just setting parameters.
+
+What data structure did you decide to store the accountability groups in and why?
+
+I initally has names input as an array, but output as a hash so I could control them as both a string and their 
+assigned integer value. 
+
+What did you learn in the process of refactoring your initial solution? Did you learn any new Ruby methods?
+
+I was unable to refactor further. 
+	
+=end
