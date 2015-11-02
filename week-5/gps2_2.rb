6beeed940209898fd58962c016f239e931ff8df9
RedZulu <ryanzell36@gmail.com>
2015-11-02 10:52:03 -0600

    gps

--- week-5/gps2_2.rb
+++ week-5/gps2_2.rb
@@ -39,10 +39,27 @@ p grocery_list
 =begin
 
 What did you learn about pseudocode from working on this challenge?
+
+Pseudocode only helps if the person you're writing it with is on the same page as you.
+
 What are the tradeoffs of using Arrays and Hashes for this challenge?
+
+Hashes were much more useful in this challenge than array. Hashes allow you to create a more defined list.
+
 What does a method return?
+
+A method calculates with an intial argument and returns a changed argument accordingly.
+
 What kind of things can you pass into methods as arguments?
+
+Arrays, hashes, integers, strings, methods, classes, and pretty much any object.
+
 How can you pass information between methods?
+
+You can use classes or call previous methods as arguments.
+
 What concepts were solidified in this challenge, and what concepts are still confusing?
+
+Hashes were given a bit more light, as well as using methods as arguments.
 
 =end
