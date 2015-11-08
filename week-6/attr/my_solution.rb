84ab278f5a724d4a1bda495010f2e94db0b110f8
Ryan Zell <ryanzell36@gmail.com>
2015-11-08 17:54:46 -0600

    attr

--- week-6/attr/my_solution.rb
+++ week-6/attr/my_solution.rb
@@ -39,43 +39,55 @@ greet.hello
 
 #release_1
 #   What are these methods doing?
-#The initialize and print_info methods are creating a #storage system for people's ages, names, and #occupations. The change methods are allowing the user #to change the instance variables.
+#The initialize and print_info methods are creating a storage system for
+#people's ages, names, and occupations. The change methods are allowing
+#the user to change the instance variables.
 
 # How are they modifying or returning the value of instance variables?
-#The change methods are reassigning the instance #variables within the class, after the print_info #class, by setting the arguments given to restatements #of each instance.
+#The change methods are reassigning the instance variables within the class,
+#after the print_info class, by setting the arguments given to restatements of each instance.
 
 #release_2
 # What changed between the last release and this release?
-#The user no longer needs to state their what_is_age #method in order to see the @age variable.
+#The user no longer needs to state their what_is_age method in order
+#to see the @age variable.
 
 # What was replaced?
-#What_is_age has been replaced by attr_reader :age. #Making it so that the value of @age can be called with .age.
+#What_is_age has been replaced by attr_reader :age. Making it so that
+#the value of @age can be called with .age.
 
 # Is this code simpler than the last?
 #Yes, attribute reader :age makes @age more visible.
 
 #release_3
 # What changed between the last release and this release?
-#The user no longer needs to create a full method to #both see and change the age instance.
+#The user no longer needs to create a full method to both see and
+#change the age instance.
 
 # What was replaced?
-#An attribute writer method for age has been added. The #change_age method was removed.
+#An attribute writer method for age has been added. The change_age
+#method was removed.
 
 # Is this code simpler than the last?
 #Yes, this code is much practical to use.
 
 #release_6
 # What is a reader method?
-#A reader method allows the user to read variables #through an entire class. It simplifies the way #variables can be seen and accessed outside of a class. #The reader method cannot make any changes to the data #in question.
+#A reader method allows the user to read variables through an entire class.
+#It simplifies the way variables can be seen and accessed outside of a class.
+#The reader method cannot make any changes to the data in question.
 
 # What is a writer method?
-#A writer method allows the user to change variables #within a class from outside of the class. It #simplifies the way variables can be accessed outside #of a class, and streamlines interactivity.
+#A write method allows the user to change variables within a class from
+#outside of the class. It simplifies the way variables can be accessed
+#outside of a class, and streamlines interactivity.
 
 # What do the attr methods do for you?
-#The attr methods apply writer, reader, or both #statuses to selected variables.
+#The attr methods apply writer, reader, or both statuses to selected variables.
 
 # Should you always use an accessor to cover your bases? Why or why not?
-#No because it could give too much power to the user. #Questions of security would come up. It could also #mess with other methods creating a buggy program.
+#No because it could give too much power to the user. Questions of security
+#would come up. It could also mess with other methods creating a buggy program.
 
 # What is confusing to you about these methods?
 #Attr methods are extremely useful and not too confusing.
